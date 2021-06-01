b59f637833ac41c3c5903e01eed0bf75a3156133
jestinboby <jestinboby@gmail.com>
2021-06-01 11:59:09 +0530

    Estimate Pi

--- estimate.py
+++ estimate.py
@@ -1,28 +1,52 @@
-def wallis(n):
-    pi = 0.0   
-    for i in range(1, n):
-        x = 4 * (i ** 2)
-        y = x - 1
-        z = float(x) / float(y)
-        if (i == 1):
-            pi = z
-        else:
-            pi *= z
-    pi *= 2
-    return pi
-n=input()
-print(wallis(n))
-
-import random
 import math
-def monte_carlo(total):
- inside = 0
- for i in range(0, total):
-  x = random.random()
-  y = random.random()
-  if math.sqrt(x**2 + y**2) < 1.0:
-    inside += 1
- pi = (float(inside) / total) * 4
+import random
+import unittest
+
+def wallis(n):
+ pi=1
+ for i in range(1,n+1):
+  pi*=(4*(i**2))/((4*(i**2))-1)
+ pi*=2
  return pi
-total=input()
-print(monte_carlo(total))
+  
+def monte_carlo(n):
+ inside=0
+ x2=0
+ y2=0
+ for i in range (1,n):
+  x2=random.random()**2
+  y2=random.random()**2
+  if(math.sqrt(x2+y2))<1.0:
+   inside+=1
+ pi=(float(inside)/n)*4
+ return pi
+
+class TestWallis(unittest.TestCase):
+    def test_low_iters(self):
+        for i in range(0, 5):
+            pi = wallis(i)
+            self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
+            
+    def test_high_iters(self):
+        for i in range(500, 600):
+            pi = wallis(i)
+            self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+
+
+class TestMC(unittest.TestCase):
+    def test_randomness(self):
+        pi0 = monte_carlo(15000)
+        pi1 = monte_carlo(15000)
+        
+        self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
+
+        self.assertFalse(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
+
+    def test_accuracy(self):
+        for i in range(500, 600):
+            pi = monte_carlo(i)
+            self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+        
+    
+if __name__ == "__main__":
+    unittest.main()   
